609b59a42a1ca1e1b57a921bb479106176a9e680
preetham <pretham123@gmail.com>
2023-02-05 13:28:43 +0530

    object class

---
+++ src/Demo.java
@@ -0,0 +1,18 @@
+
+public class Demo {
+
+	public String toString() {
+		
+		return "I LOVE YOU";
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Demo d=new Demo();
+		System.out.println(d);
+		
+		
+	}
+
+}
---
+++ src/Demo1.java
@@ -0,0 +1,15 @@
+
+public class Demo1 {
+	
+	public int hashcode()
+	{
+		return 1456;
+		
+	}
+	public static void main(String[] args) {
+		Demo1 d=new Demo1();
+		System.out.println(d.hashcode());
+		
+	}
+
+}
